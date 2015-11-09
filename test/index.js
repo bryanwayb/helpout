b6d9dc6dbb23e070e965263cb3faa78da2b09445
Bryan Way <bryanwayb@gmail.com>
2015-11-09 03:14:34 -0600

    Added readme and a few small modifications

--- test/index.js
+++ test/index.js
@@ -2,8 +2,7 @@ var npmPackage = require('../package.json'),
     helpout = require('../lib/index.js');
 
 process.stdout.write(helpout.version(npmPackage));
-
-process.stdout.write(require('../lib/index.js').help({
+process.stdout.write(helpout.help({
     npmPackage: npmPackage,
     usage: [
         '<command> [options]',
@@ -14,8 +13,8 @@ process.stdout.write(require('../lib/index.js').help({
         Details: {
             description: 'This is a test description. The content here should be wrapped to the console width and only on word breaks before the end of the line.',
             options: {
-                '--test, -t': 'Toggles a test switch. Literally does nothing, but nice to look at when passing a command',
-                '-o': 'Another test switch, but this time a bit more circular and minimal in appearance. All the rage with the hipsters.',
+                '--test, -t': 'Toggles a test switch. Literally does nothing, but nice to look at when passing a command.',
+                '-o': 'Another test switch, but this time a bit more circular and minimal in appearance. All the rage with hipsters.',
                 '--a-really-frickin-long-switch': 'Really pushing the limits of what is classified as a valid switch now, are we?'
             }
         }
